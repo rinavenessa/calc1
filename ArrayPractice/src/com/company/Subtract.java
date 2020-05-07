0cb5230d1f62692602778ef6c247d32c71137ffe
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-05-07 14:13:30 +0200

    Update Subtract.java

--- ArrayPractice/src/com/company/Subtract.java
+++ ArrayPractice/src/com/company/Subtract.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Subtract {
-    public static double subtract(double a, double b){
+    public double subtract(double a, double b){
         double answer = a - b;
         System.out.println(answer);
         return answer;
