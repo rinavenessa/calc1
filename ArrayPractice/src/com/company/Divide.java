55206308015bb5ecc956cf21de66ee89892e12f2
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-05-07 14:10:54 +0200

    Update Divide.java

--- ArrayPractice/src/com/company/Divide.java
+++ ArrayPractice/src/com/company/Divide.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Divide {
-    public static double divide(double a, double b) {
+    public double divide(double a, double b) {
         double answer = a / b;
 
         System.out.println(answer);
