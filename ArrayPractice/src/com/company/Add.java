33b1acf7df38b35bb6be919ec182f7c2cedae2de
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-05-07 14:10:14 +0200

    Update Add.java

--- ArrayPractice/src/com/company/Add.java
+++ ArrayPractice/src/com/company/Add.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Add {
-    public static double add(double a, double b){
+    public double add(double a, double b){
         double answer = a + b;
         System.out.println(answer);
         return answer;
